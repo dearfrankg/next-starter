8963390a33aa8874182c297f73a9353969a10edd
Frank Gutierrez <dearfrankg@gmail.com>
2024-07-14 23:33:33 -0700

    add json util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function json(data: any) {
+  console.log(JSON.stringify(data, null, 2));
 }
