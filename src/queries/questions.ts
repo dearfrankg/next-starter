0ff83470dfe5f9d7933bab1e2ef8b85666838ec9
Frank Gutierrez <dearfrankg@gmail.com>
2024-07-27 04:15:04 -0700

    add shadcn-ui components

--- src/queries/questions.ts
+++ src/queries/questions.ts
@@ -2,8 +2,15 @@ import prisma from "@/lib/prisma";
 
 export async function getTest({ userId }: { userId: string }) {
   return await prisma.test.findMany({
-    where: {
-      userId,
+    select: {
+      topic: true,
+      description: true,
+      creator: {
+        select: {
+          name: true,
+          email: true,
+        },
+      },
     },
   });
 }
