b6f3a3d247ad97b293663e3ae89a971a14ed50e7
Frank Gutierrez <dearfrankg@gmail.com>
2024-07-29 08:31:32 -0700

    update queries

---
+++ src/queries/test-attempts.ts
@@ -0,0 +1,48 @@
+import prisma from "@/lib/prisma";
+import { SearchParamProps } from "@/types";
+import { TestAttemptTableItem } from "@/types/prisma";
+
+export async function getLastestTestAttempts({
+  userId,
+  searchParams,
+}: { userId: string } & SearchParamProps) {
+  //
+
+  const { selectedTest: testId } = searchParams;
+
+  if (!userId || !testId) {
+    return [];
+  }
+
+  return prisma.testAttempt.findMany({
+    where: {
+      userId,
+      testId,
+    },
+    orderBy: {
+      startedAt: "desc",
+    },
+    take: 8,
+    select: {
+      id: true,
+      startedAt: true,
+      totalCount: true,
+      correctCount: true,
+      percentage: true,
+      user: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+      test: {
+        select: {
+          id: true,
+          topic: true,
+        },
+      },
+    },
+  });
+
+  //
+}
---
+++ src/queries/tests.ts
@@ -0,0 +1,103 @@
+import prisma from "@/lib/prisma";
+import { SearchParamProps } from "@/types";
+import { TestWithCreator } from "@/types/prisma";
+
+export interface GetPagedTestsReturns {
+  tests: TestWithCreator[];
+  count: number;
+  page: number;
+  totalPages: number;
+}
+
+export async function getPagedTests({
+  userId,
+  searchParams,
+}: { userId: string } & SearchParamProps): Promise<GetPagedTestsReturns> {
+  //
+
+  async function getCount({
+    userId,
+    query,
+  }: {
+    userId: string;
+    query: string;
+  }) {
+    return prisma.test.count({
+      where: {
+        AND: [
+          { topic: { contains: query, mode: "insensitive" } },
+          {
+            testAttempts: {
+              some: { userId },
+            },
+          },
+        ],
+      },
+    });
+  }
+
+  async function getRecords({
+    userId,
+    query,
+    skip,
+    take,
+  }: {
+    userId: string;
+    query: string;
+    skip: number;
+    take: number;
+  }) {
+    return prisma.test.findMany({
+      where: {
+        AND: [
+          { topic: { contains: query, mode: "insensitive" } },
+          {
+            testAttempts: {
+              some: { userId },
+            },
+          },
+        ],
+      },
+      select: {
+        id: true,
+        topic: true,
+        description: true,
+        creator: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+      },
+      orderBy: {
+        topic: "asc",
+      },
+      skip,
+      take,
+    });
+  }
+
+  const { query = "", pageSize = "8" } = searchParams;
+  const count: number = await getCount({ userId, query });
+
+  const totalPages: number = Math.ceil(count / Number(pageSize));
+
+  // cap page at total pages
+  let page: number = Number(searchParams.page || "1");
+  page = page > totalPages ? totalPages : page;
+
+  const skip: number = (page - 1) * Number(pageSize);
+  const take: number = Number(pageSize);
+  const tests = count ? await getRecords({ userId, query, skip, take }) : [];
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  return {
+    tests,
+    count,
+    page,
+    totalPages,
+  };
+
+  //
+}
