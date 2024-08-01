f81df57fa8fc8d1caa640d7cb8da4e15049aa485
Frank Gutierrez <dearfrankg@gmail.com>
2024-08-01 07:54:05 -0700

    fix tailwind rounded sizes

--- src/components/pages/(learning)/dashboard/layout.tsx
+++ src/components/pages/(learning)/dashboard/layout.tsx
@@ -12,12 +12,12 @@ export function DashboardLayout({
   return (
     <div className="flex w-full flex-col gap-8">
       <div className="flex flex-wrap gap-8">
-        <div className="flex-1 text-nowrap rounded-md border-2 p-4">{left}</div>
-        <div className="flex-1 text-nowrap rounded-md border-2 p-4">
+        <div className="flex-1 text-nowrap rounded-lg border-2 p-4">{left}</div>
+        <div className="flex-1 text-nowrap rounded-lg border-2 p-4">
           {right}
         </div>
       </div>
-      <div className="flex-1 text-nowrap rounded-md border-2 p-4">{bottom}</div>
+      <div className="flex-1 text-nowrap rounded-lg border-2 p-4">{bottom}</div>
     </div>
   );
 }
--- src/components/pages/(learning)/generate/layout.tsx
+++ src/components/pages/(learning)/generate/layout.tsx
@@ -11,8 +11,8 @@ export function AITestGeneratorLayout({
   return (
     <div className="flex w-full flex-col gap-8">
       <div className="flex flex-wrap gap-8">
-        <div className="flex-1 text-nowrap rounded-md border-2 p-4">{left}</div>
-        <div className="flex-1 text-nowrap rounded-md border-2 p-4">
+        <div className="flex-1 text-nowrap rounded-lg border-2 p-4">{left}</div>
+        <div className="h-[664px] flex-1 text-nowrap rounded-lg border-2 p-4">
           {right}
         </div>
       </div>
